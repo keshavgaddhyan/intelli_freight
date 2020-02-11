088451c7386801cb5e8a94762ec74a2cdfb94f38
Keshav Gaddhyan <keshavgaddhyan@gmail.com>
2020-02-11 03:24:53 -0500

    azure

--- job_hack/settings.py
+++ job_hack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=2gt@uz8ttdf9=iv)71lpc2v_l%_)f0k7#!$tlv3cny#h^1ysy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
