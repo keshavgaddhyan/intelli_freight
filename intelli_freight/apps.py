65715fe956090905ffc49e334c7aa1615293260b
Keshav Gaddhyan <keshavgaddhyan@gmail.com>
2020-02-11 00:15:28 -0500

    First commit

---
+++ intelli_freight/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IntelliFreightConfig(AppConfig):
+    name = 'intelli_freight'
---
+++ intelli_freight/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.urls import path
+from intelli_freight import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
